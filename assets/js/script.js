a1204fe31cd6d0da0af3176dde8f228e84877874
John Guzzetta <johnguzzettaiv@gmail.com>
2022-03-31 18:01:47 -0400

    Added window prompts for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // Arrays for special characters, lowercase letters, uppercase letters, and numbers
-constspecialChars=["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+const specialChars=["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
 
-constnumberChars=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const numberChars=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-constuppercase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const uppercase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-constlowercase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowercase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var possibleChars = []
 
 // Assignment Code-------------------------------------------------//
 var generateBtn = document.querySelector("#generate");
@@ -24,7 +25,41 @@ generateBtn.addEventListener("click", writePassword);
 
 // This function will generate my password in full
 function generatePassword(){
-  var promptSelections = prompt()
+  // the parseInt returns the user's input as a Number stored to promptSelections
+var promptSelections = parseInt(prompt("How many characters, between 8 and 32, do you wish to use?"));
+  console.log(typeof promptSelections);
+  console.log(promptSelections);
+
+      //------Below are my prompts asking users for which items they wish to include------//
+  //--------------------------------------------------------
+  promptSelections = confirm("Would you like to include special characters within your password?")
+    // If promptSelections is confirmed, this adds the specialChars array to possibleChars, if cancelled, it will not add the arrays
+  if(promptSelections){    
+    possibleChars = possibleChars.concat(specialChars)
+    }
+  //--------------------------------------------------------
+  promptSelections = confirm("Would you like to include numerical characters within your password?")
+    // If promptSelections is confirmed, this adds the numberChars array to possibleChars
+  if(promptSelections){    
+    possibleChars = possibleChars.concat(numberChars)
+    }
+  //--------------------------------------------------------
+  promptSelections = confirm("Would you like to include Uppercase characters within your password?")
+    // If promptSelections is confirmed, this adds the uppercase array to possibleChars
+  if(promptSelections){    
+    possibleChars = possibleChars.concat(uppercase)
+    }
+  //--------------------------------------------------------
+  promptSelections = confirm("Would you like to include Lowercase characters within your password?")
+    // If promptSelections is confirmed, this adds the lowercase array to possibleChars
+  if(promptSelections){    
+    possibleChars = possibleChars.concat(lowercase)
+    }
+  //--------------------------------------------------------
 
 
+console.log();
+console.log(possibleChars);
+
+return possibleChars;
 };
